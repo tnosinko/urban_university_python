4053b0ac6081c8217859d8a7ce0cf33cef6e6c58
tnosinko <tnosinko@gmail.com>
2024-11-12 10:38:38 +0300

    fix code 12.11.2024

---
+++ additional_tasks_modile_1.py
@@ -0,0 +1,14 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+student_assessment = dict()
+
+students1 = float(sum(grades[0]) / len(grades[0]))
+students2 = float(sum(grades[1]) / len(grades[1]))
+students3 = float(sum(grades[2]) / len(grades[2]))
+students4 = float(sum(grades[3]) / len(grades[3]))
+students5 = float(sum(grades[4]) / len(grades[4]))
+
+sort_students = sorted(students)
+student_assessment.update(({sort_students[0]: students1, sort_students[1]: students2, sort_students[2]: students3,
+                            sort_students[3]: students4, sort_students[4]: students5}))
+print(student_assessment)
