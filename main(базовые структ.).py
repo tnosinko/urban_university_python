a424839299a3e0e0f0e71e146c89e3aa118acdc1
tnosinko <tnosinko@gmail.com>
2024-11-12 10:45:28 +0300

    fix code 12.11.2024

---
+++ main(базовые структ.).py
@@ -0,0 +1,44 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print ("Hello Word")
+
+print ('1st program')
+print (9**0.5 *(5))
+
+print ('2nd program')
+print (9.99>9.98 and 1000!=1000.1)
+
+print ('3rd program')
+print (2*2+2)
+print (2*(2+2))
+print (2*2+2 == 2*(2+2))
+
+print ('4th program')
+name = '123.456'
+print (name [4])
+
+a = "123.456"
+b = float (a)
+print (type (b))
+c = b*10
+print (c)
+c =int(c)
+print (c%10)
+
+#или другое решение 4 задачи
+
+print (int(float('123.456')*10)%10)
