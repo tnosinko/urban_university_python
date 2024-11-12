f11975d0052001dc369866c18fc363074107c911
tnosinko <tnosinko@gmail.com>
2024-11-12 10:42:14 +0300

    fix code 12.11.2024 --- line 11 add commit

--- additional_tasks_modile_1.py
+++ additional_tasks_modile_1.py
@@ -8,7 +8,7 @@ students3 = float(sum(grades[2]) / len(grades[2]))
 students4 = float(sum(grades[3]) / len(grades[3]))
 students5 = float(sum(grades[4]) / len(grades[4]))
 
-sort_students = sorted(students)
+sort_students = sorted(students) # Исправила имена переменных
 student_assessment.update(({sort_students[0]: students1, sort_students[1]: students2, sort_students[2]: students3,
                             sort_students[3]: students4, sort_students[4]: students5}))
 print(student_assessment)
