114539b749f87c01b2fad331a088be67d77ed7b0
tnosinko <tnosinko@gmail.com>
2024-11-19 13:36:14 +0300

    19.11.2024 я вообще не поняла эту тему с while,и не понимаю что куда вписать даже из ваших комментариев,так как записываю что вы указали.у меня программа выдает ошибку.

--- module_2_3(цикл while).py
+++ module_2_3(цикл while).py
@@ -1,9 +1,10 @@
-my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
-while 1 > 0:
-    my_list =int(input('введите число'))
-    if my_list %2==0:
-        print('положительное число')
-    else:
-        print('отрицательное число')
-        print('конец')
-        break
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+
+while index < len(my_list):
+        if my_list[index] < 0:
+            if my_list[index] > 0:
+                if my_list[index] == 0:
+            break
+            continue
+        print(my_list[index])
