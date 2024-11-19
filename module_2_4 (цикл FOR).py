f88059e3fa075d3623a38dcc9f10965d176f6f3e
tnosinko <tnosinko@gmail.com>
2024-11-19 14:14:07 +0300

    19.11.2024 исправлено

--- module_2_4 (цикл FOR).py
+++ module_2_4 (цикл FOR).py
@@ -2,17 +2,10 @@ numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 primes=[]
 not_primes=[]
 
-for i in numbers:
-     print(numbers)
-
-
- new_ = int(input('введите число n:'))
-
 for i in range(len(numbers)):
-    is_primes = True
+     is_primes = True
      n = numbers[i]
      if n < 2:
-         print(n)
          continue
      else:
          f = n ** (1 / 2)
