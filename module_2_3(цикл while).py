f1eb7bd19cd0b9d1c6239099f4f6e3c48878c7f1
tnosinko <tnosinko@gmail.com>
2024-11-19 14:03:18 +0300

    19.11.2024 изменила, но что-то не так работает цикл, он не останавливается

--- module_2_3(цикл while).py
+++ module_2_3(цикл while).py
@@ -1,10 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
-
+i=1
 while index < len(my_list):
         if my_list[index] < 0:
-            if my_list[index] > 0:
-                if my_list[index] == 0:
             break
+        if my_list[index] > 0:
+            print('число', index + i)
+
+        if my_list[index] == 0:
+            (index + i)
             continue
         print(my_list[index])
